82cebd52456da35fbd0d7ec6550eb62ad8e15efb
Adithyan-06 <adithyanmnair6@gmail.com>
2025-07-25 19:09:32 +0530

    commitss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Route, Routes } from "react-router";
 import { SignUpForm } from './pages/signup';
+import Navbar from './components/navbar';
+import Home from './pages/homepage';
 
 
 const App = () => {
   return(
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/signup" element={<SignUpForm />}/>
+        <Route path="/" element={<Home />}/>
       </Routes>
     </div>
   )
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white dark:bg-gray-900 shadow-md">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          
+          <Link
+            to="/"
+            className="text-2xl font-bold text-blue-600 dark:text-white"
+          >
+            JobSeek
+          </Link>
+
+          
+          <div className="flex items-center ">
+            <Link
+              to="/login"
+              className="text-blue-600 hover:text-white border border-blue-600 hover:bg-blue-800 transition ease-in-out duration-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2  text-center ml-2 me-2 mb-0.5"
+            >
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="text-white bg-blue-600 hover:bg-blue-700  focus:outline-none transition ease-in-out duration-300 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-0.5"
+            >
+              Register
+            </Link>           
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
